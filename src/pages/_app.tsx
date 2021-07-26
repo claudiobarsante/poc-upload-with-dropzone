7c69b2e7d276c92da2435c0740390d4d0b5b60ee
Claudio Barsante <clbmribas@gmail.com>
2021-07-26 12:26:46 -0300

    fix(react-circular.scss added for react-circular-progressbar  work on Nextjs)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { UploadProvider } from '../context/UploadContext';
 import GlobalStyles from '../styles/global';
-
+import '../styles/react-circular.scss';
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<UploadProvider>
